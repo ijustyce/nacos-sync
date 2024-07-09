fd629faa93b5d8e848ab1b737ee2fc3417ab1579
yangchun2 <yangchun2@shopline.com>
2024-07-09 14:25:08 +0800

    use ip

--- nacossync-worker/src/main/java/com/alibaba/nacossync/task/ClusterTask.java
+++ nacossync-worker/src/main/java/com/alibaba/nacossync/task/ClusterTask.java
@@ -90,57 +90,57 @@ public class ClusterTask implements CommandLineRunner {
     }
 
     private void addAllCluster() {
-        addCluster("fs-test-01", "nacos-test-fs.inshopline.com:6801", "");
-        addCluster("fs-test-01-test", "nacos-test-fs.inshopline.com:6801", "test");
-        addCluster("fs-test-01-deve", "nacos-test-fs.inshopline.com:6801", "develop");
-        addCluster("fs-test-01-local", "nacos-test-fs.inshopline.com:6801", "local");
-        addCluster("fs-test-01-press", "nacos-test-fs.inshopline.com:6801", "press");
-        addCluster("fs-test-01-sandbox", "nacos-test-fs.inshopline.com:6801", "sandbox");
-        addCluster("fs-test-01-sl-ecom-old-test", "nacos-test-fs.inshopline.com:6801",
+        addCluster("fs-test-01", "10.98.64.84:6801", "");
+        addCluster("fs-test-01-test", "10.98.64.84:6801", "test");
+        addCluster("fs-test-01-deve", "10.98.64.84:6801", "develop");
+        addCluster("fs-test-01-local", "10.98.64.84:6801", "local");
+        addCluster("fs-test-01-press", "10.98.64.84:6801", "press");
+        addCluster("fs-test-01-sandbox", "10.98.64.84:6801", "sandbox");
+        addCluster("fs-test-01-sl-ecom-old-test", "10.98.64.84:6801",
                 "68364d5e-c6e0-4012-b6d2-c9106a688932");
-        addCluster("fs-test-01-sl-ecom-new-test", "nacos-test-fs.inshopline.com:6801",
+        addCluster("fs-test-01-sl-ecom-new-test", "10.98.64.84:6801",
                 "21ffcfab-d33d-4764-97ad-94de7f64493d");
-        addCluster("fs-test-01-sl-ecom-new-dev", "nacos-test-fs.inshopline.com:6801",
+        addCluster("fs-test-01-sl-ecom-new-dev", "10.98.64.84:6801",
                 "4636c623-93b6-472b-8d48-f482aed74c98");
-        addCluster("fs-test-01-tester-test", "nacos-test-fs.inshopline.com:6801", "tester-test");
-        addCluster("fs-test-01-sl-jdp", "nacos-test-fs.inshopline.com:6801", "sl-jdp");
-        addCluster("fs-test-01-data_platform", "nacos-test-fs.inshopline.com:6801", "data_platform");
-        addCluster("fs-test-01-sl-ecom-shopify-test", "nacos-test-fs.inshopline.com:6801", "7758344d-2c6b-487e-8eed-c78b97fc1f1f");
-        addCluster("fs-test-01-sl-ecom-others-test", "nacos-test-fs.inshopline.com:6801", "bcdbf3ec-b0f4-4d8e-9156-36a3c19dc2e2");
-        addCluster("fs-test-01-slp-local", "nacos-test-fs.inshopline.com:6801", "slp-local");
-        addCluster("fs-test-01-slp-test", "nacos-test-fs.inshopline.com:6801", "slp-test");
-        addCluster("fs-test-01-slp-develop", "nacos-test-fs.inshopline.com:6801", "slp-develop");
-        addCluster("fs-test-01-sl-ecom-open-test", "nacos-test-fs.inshopline.com:6801", "8209e0ba-d4d7-41a0-b8e3-4c96e2faf15e");
-        addCluster("fs-test-01-slp-sandbox", "nacos-test-fs.inshopline.com:6801", "slp-sandbox");
-        addCluster("fs-test-01-sale-press", "nacos-test-fs.inshopline.com:6801", "sale-press");
-        addCluster("fs-test-01-sales-af-press", "nacos-test-fs.inshopline.com:6801", "sales-af-press");
-        addCluster("fs-test-01-product-press", "nacos-test-fs.inshopline.com:6801", "product-press");
+        addCluster("fs-test-01-tester-test", "10.98.64.84:6801", "tester-test");
+        addCluster("fs-test-01-sl-jdp", "10.98.64.84:6801", "sl-jdp");
+        addCluster("fs-test-01-data_platform", "10.98.64.84:6801", "data_platform");
+        addCluster("fs-test-01-sl-ecom-shopify-test", "10.98.64.84:6801", "7758344d-2c6b-487e-8eed-c78b97fc1f1f");
+        addCluster("fs-test-01-sl-ecom-others-test", "10.98.64.84:6801", "bcdbf3ec-b0f4-4d8e-9156-36a3c19dc2e2");
+        addCluster("fs-test-01-slp-local", "10.98.64.84:6801", "slp-local");
+        addCluster("fs-test-01-slp-test", "10.98.64.84:6801", "slp-test");
+        addCluster("fs-test-01-slp-develop", "10.98.64.84:6801", "slp-develop");
+        addCluster("fs-test-01-sl-ecom-open-test", "10.98.64.84:6801", "8209e0ba-d4d7-41a0-b8e3-4c96e2faf15e");
+        addCluster("fs-test-01-slp-sandbox", "10.98.64.84:6801", "slp-sandbox");
+        addCluster("fs-test-01-sale-press", "10.98.64.84:6801", "sale-press");
+        addCluster("fs-test-01-sales-af-press", "10.98.64.84:6801", "sales-af-press");
+        addCluster("fs-test-01-product-press", "10.98.64.84:6801", "product-press");
 
-        addCluster("fs-loong-01", "loong-fs-test.inshopline.com:6801", "");
-        addCluster("fs-loong-01-test", "loong-fs-test.inshopline.com:6801", "test");
-        addCluster("fs-loong-01-deve", "loong-fs-test.inshopline.com:6801", "develop");
-        addCluster("fs-loong-01-local", "loong-fs-test.inshopline.com:6801", "local");
-        addCluster("fs-loong-01-press", "loong-fs-test.inshopline.com:6801", "press");
-        addCluster("fs-loong-01-sandbox", "loong-fs-test.inshopline.com:6801", "sandbox");
-        addCluster("fs-loong-01-sl-ecom-old-test", "loong-fs-test.inshopline.com:6801",
+        addCluster("fs-loong-01", "10.98.64.94:6801", "");
+        addCluster("fs-loong-01-test", "10.98.64.94:6801", "test");
+        addCluster("fs-loong-01-deve", "10.98.64.94:6801", "develop");
+        addCluster("fs-loong-01-local", "10.98.64.94:6801", "local");
+        addCluster("fs-loong-01-press", "10.98.64.94:6801", "press");
+        addCluster("fs-loong-01-sandbox", "10.98.64.94:6801", "sandbox");
+        addCluster("fs-loong-01-sl-ecom-old-test", "10.98.64.94:6801",
                 "68364d5e-c6e0-4012-b6d2-c9106a688932");
-        addCluster("fs-loong-01-sl-ecom-new-test", "loong-fs-test.inshopline.com:6801",
+        addCluster("fs-loong-01-sl-ecom-new-test", "10.98.64.94:6801",
                 "21ffcfab-d33d-4764-97ad-94de7f64493d");
-        addCluster("fs-loong-01-sl-ecom-new-dev", "loong-fs-test.inshopline.com:6801",
+        addCluster("fs-loong-01-sl-ecom-new-dev", "10.98.64.94:6801",
                 "4636c623-93b6-472b-8d48-f482aed74c98");
-        addCluster("fs-loong-01-tester-test", "loong-fs-test.inshopline.com:6801", "tester-test");
-        addCluster("fs-loong-01-sl-jdp", "loong-fs-test.inshopline.com:6801", "sl-jdp");
-        addCluster("fs-loong-01-data_platform", "loong-fs-test.inshopline.com:6801", "data_platform");
-        addCluster("fs-loong-01-sl-ecom-shopify-test", "loong-fs-test.inshopline.com:6801", "7758344d-2c6b-487e-8eed-c78b97fc1f1f");
-        addCluster("fs-loong-01-sl-ecom-others-test", "loong-fs-test.inshopline.com:6801", "bcdbf3ec-b0f4-4d8e-9156-36a3c19dc2e2");
-        addCluster("fs-loong-01-slp-local", "loong-fs-test.inshopline.com:6801", "slp-local");
-        addCluster("fs-loong-01-slp-test", "loong-fs-test.inshopline.com:6801", "slp-test");
-        addCluster("fs-loong-01-slp-develop", "loong-fs-test.inshopline.com:6801", "slp-develop");
-        addCluster("fs-loong-01-sl-ecom-open-test", "loong-fs-test.inshopline.com:6801", "8209e0ba-d4d7-41a0-b8e3-4c96e2faf15e");
-        addCluster("fs-loong-01-slp-sandbox", "loong-fs-test.inshopline.com:6801", "slp-sandbox");
-        addCluster("fs-loong-01-sale-press", "loong-fs-test.inshopline.com:6801", "sale-press");
-        addCluster("fs-loong-01-sales-af-press", "loong-fs-test.inshopline.com:6801", "sales-af-press");
-        addCluster("fs-loong-01-product-press", "loong-fs-test.inshopline.com:6801", "product-press");
+        addCluster("fs-loong-01-tester-test", "10.98.64.94:6801", "tester-test");
+        addCluster("fs-loong-01-sl-jdp", "10.98.64.94:6801", "sl-jdp");
+        addCluster("fs-loong-01-data_platform", "10.98.64.94:6801", "data_platform");
+        addCluster("fs-loong-01-sl-ecom-shopify-test", "10.98.64.94:6801", "7758344d-2c6b-487e-8eed-c78b97fc1f1f");
+        addCluster("fs-loong-01-sl-ecom-others-test", "10.98.64.94:6801", "bcdbf3ec-b0f4-4d8e-9156-36a3c19dc2e2");
+        addCluster("fs-loong-01-slp-local", "10.98.64.94:6801", "slp-local");
+        addCluster("fs-loong-01-slp-test", "10.98.64.94:6801", "slp-test");
+        addCluster("fs-loong-01-slp-develop", "10.98.64.94:6801", "slp-develop");
+        addCluster("fs-loong-01-sl-ecom-open-test", "10.98.64.94:6801", "8209e0ba-d4d7-41a0-b8e3-4c96e2faf15e");
+        addCluster("fs-loong-01-slp-sandbox", "10.98.64.94:6801", "slp-sandbox");
+        addCluster("fs-loong-01-sale-press", "10.98.64.94:6801", "sale-press");
+        addCluster("fs-loong-01-sales-af-press", "10.98.64.94:6801", "sales-af-press");
+        addCluster("fs-loong-01-product-press", "10.98.64.94:6801", "product-press");
     }
 
     private void addCluster(String name, String address, String namespace) {
